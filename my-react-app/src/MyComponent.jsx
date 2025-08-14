b7df3d4f3df3f980165b93bfcd5f4a952f32e56a
jaysinhzala <jaysinhzala1@gmail.com>
2025-08-14 14:02:40 -0400

    update array in state

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
 
 function MyComponent() {
-    const [name, setName] = useState('Guest');
+    const [foods, setFoods] = useState(["Orange", "Apple", "Banana"]);
 
-    function handleNameChange(event) {
-        setName(event.target.value);
+    function handleAddFood(){
+        const newFood = document.getElementById('newFood').value;
+        document.getElementById('newFood').value = "";
+        setFoods(f => [...f,newFood]);
     }
-    return (<>
-        <input value={name} onChange={handleNameChange} />
-        <p>Name:{name}</p>
-    </>
-    )
+    
+    function handleRemoveFood(index){
+        setFoods(f => f.filter((_,i) => i !== index));
+    }
+
+    return (<div>
+        <h1>List of Food</h1>
+        <ul>
+            {foods.map((food, index) => <li key={index} onClick= {() =>{handleRemoveFood(index)}} >{food}</li>)}
+        </ul>
+        <input type='text' id='newFood' placeholder="Enter your food"/>
+        <button onClick={handleAddFood}>Add</button>
+    </div>);
 }
 
 export default MyComponent;
