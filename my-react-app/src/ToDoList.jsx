db60c10c946adc9d8e87e748354fafe4055e2ad8
jaysinhzala <jaysinhzala1@gmail.com>
2025-08-20 15:26:44 -0400

    added functioality

--- my-react-app/src/ToDoList.jsx
+++ my-react-app/src/ToDoList.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function ToDoList() {
-    const [tasks, setTasks] = useState(["Walk Dog","Get Grocceries"]);
+    const [tasks, setTasks] = useState(["Walk Dog", "Get Grocceries"]);
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event) {
@@ -9,45 +9,60 @@ function ToDoList() {
     }
 
     function addTasks() {
-        setTasks = newTask;
+        if(newTask.trim() !== ""){  
+            setTasks(t => [...t,newTask]);
+            setNewTask("");
+        }
     }
 
     function deleteTask(index) {
-
+        const updatedTasks = tasks.filter((_, i) => i != index);
+        setTasks(updatedTasks);
     }
 
     function moveTaskUp(index) {
-
+        if(index > 0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index],updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
     function moveTaskDown(index) {
-
+        if(index < tasks.length - 1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index],updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
     return (<div className="to-do-list">
-        <h1>To-Do-List</h1>
-        
-        <input type="text"
-            placeholder="Enter text here"
-            value={newTask}
-            onChange={handleInputChange}>
-        </input>
+        <div className="to-do-list-inner">
+            <h1>To-Do-List</h1>
+            <div className="to-do-list-back">
+                <input type="text"
+                    placeholder="Enter text here"
+                    value={newTask}
+                    onChange={handleInputChange}>
+                </input>
 
-        <button onClick={addTasks}>Add</button>
+                <button onClick={addTasks}>Add</button>
 
-        <ol>
-            {tasks.map((task, index) =>
-            <li key={index}>
-                <span className="text">{task}</span>
-                <button className="delte-btn" onClick={() => deleteTask(index)}>Delete</button>
+                <ol>
+                    {tasks.map((task, index) =>
+                        <li key={index}>
+                            <span className="text">{task}</span>
+                            <button className="delte-btn" onClick={() => deleteTask(index)}>Delete</button>
 
-                <button className="move-up-btn" onClick={() => moveTaskUp(index)}>👆</button>
+                            <button className="move-up-btn" onClick={() => moveTaskUp(index)}>👆</button>
 
-                <button className="move-down-btn" onClick={() => moveTaskDown(index)}>👇</button>
-            </li>)}
-        </ol>
+                            <button className="move-down-btn" onClick={() => moveTaskDown(index)}>👇</button>
+                        </li>)}
+                </ol>
+            </div>
+        </div>
 
-    </div>);
+    </div>)
 }
 
 export default ToDoList;
