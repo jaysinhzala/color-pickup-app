81ad39fdc3b9fe7d9f21ec035a12820c76bd1208
jaysinhzala <jaysinhzala1@gmail.com>
2025-08-21 12:57:45 -0400

    implied logic behind stopwatch

--- my-react-app/src/Stopwatch.jsx
+++ my-react-app/src/Stopwatch.jsx
@@ -7,23 +7,45 @@ function Stopwatch(){
     const startTimeRef = useRef(0);
 
     useEffect(() => {
+        if(isRunning){
+            intervalIdRef.current = setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current);
+            },10);
+        }
+
+        return () => {
+            clearInterval(intervalIdRef.current); 
+        }
 
     }, [isRunning]);
 
     function start(){
-
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
     }
 
     function stop(){
-
+        setIsRunning(false);
     }
 
     function reset(){
-
+        setElapsedTime(0);
+        setIsRunning(false);
     }
 
     function formateTime(){
-        return `00:00:00`;
+
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60 );
+        let seconds = Math.floor(elapsedTime / (1000) % 60);
+        let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+        hours = String(hours).padStart(2,"0");
+        minutes = String(minutes).padStart(2,"0");
+        seconds = String(seconds).padStart(2,"0");
+        milliseconds = String(milliseconds).padStart(2,"0");
+
+        return `${hours}:${minutes}:${seconds}:${milliseconds}`;
     }
 
     return(<div className="stopwatch">
